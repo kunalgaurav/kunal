987cfecd1742a2ec71c61867c23b170c4acafe19
kunal <kunal@kunal-Vostro-3800>
2016-06-30 17:16:05 +0530

    2nd commit

---
+++ src/newclass.cpp
@@ -0,0 +1,18 @@
+/*
+ * newclass.cpp
+ *
+ *  Created on: 30-Jun-2016
+ *      Author: kunal
+ */
+
+#include "newclass.h"
+
+newclass::newclass() {
+	// TODO Auto-generated constructor stub
+
+}
+
+newclass::~newclass() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/newclass.h
@@ -0,0 +1,17 @@
+/*
+ * newclass.h
+ *
+ *  Created on: 30-Jun-2016
+ *      Author: kunal
+ */
+
+#ifndef NEWCLASS_H_
+#define NEWCLASS_H_
+
+class newclass {
+public:
+	newclass();
+	virtual ~newclass();
+};
+
+#endif /* NEWCLASS_H_ */
